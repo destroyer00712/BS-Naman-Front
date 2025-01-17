b17ca033981c83a702dd8eb85c8643e148257db8
Ashagana2014 <namansetty02@gmail.com>
2025-01-17 22:43:13 +0530

    no fix

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -3,7 +3,6 @@ import config from '../modules/config';
 import { Info, Plus, Send, Mic } from 'lucide-react';
 import WorkerModal from './WorkerModal';
 import VoiceMessageDialog from './VoiceMessageDialog';
-import fetch from "cross-fetch";
 
 const SendMessageModal = ({ 
   show, 
