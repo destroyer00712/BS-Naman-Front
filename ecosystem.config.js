87b1ff3df22fe1db1d84d71d299c94ad9c61d7cb
Naman Setty <namansetty02@gmail.com>
2025-04-04 16:27:14 +0530

    Update ecosystem configuration to rename app from "my-react-app" to "BS Gold Frontend" and change the serve port from 1344 to 1341, ensuring proper deployment settings.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 // ecosystem.config.js
 module.exports = {
     apps: [{
-      name: "my-react-app",
+      name: "BS Gold Frontend",
       script: "serve",
       env: {
         PM2_SERVE_PATH: './build',
-        PM2_SERVE_PORT: 1344,
+        PM2_SERVE_PORT: 1341,
         PM2_SERVE_SPA: 'true',
         PM2_SERVE_HOMEPAGE: '/index.html'
       }
